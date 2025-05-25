3f3a858497e63d419be461a66fab3bf8dfeca5be
Richard Melvin <rgmelvin@pop-os.localdomain>
2025-05-25 11:32:41 -0500

    Suppress upstream ESM deprecation warning until ts-node fixes

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -136,7 +136,7 @@ if [[ -z "$EMBEDDED_ID" ]]; then
 else
   echo "📦 Confirmed: Rebuilt IDL contains program ID: $EMBEDDED_ID"
   echo "⏳ Waiting for validator to recognize deployed program ID via simulation..."
-  node --import ts-node/esm "$BEARGREASE_ROOT/scripts/wait-for-program.mts"
+  NODE_OPTIONS="--no-warnings" npx ts-node-esm"$BEARGREASE_ROOT/scripts/wait-for-program.mts" # NODE_OPTIONS supresses a deprecation warning that we will have to watch
 fi
 
 # ---------------------------------------------------------------
